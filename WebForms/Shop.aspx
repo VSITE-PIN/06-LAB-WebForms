d184a5a358d4b1017299ce40317a0e9b51c03099
Lovre Kešina <lokesina@vsite.hr>
2025-01-20 21:15:08 +0100

    lovrekes

---
+++ WebForms/Shop.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="Shop" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeBehind="Shop.aspx.cs" Inherits="YourNamespace.Shop" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>Shop</h2>
+    <table>
+        <tr>
+            <td>Naziv:</td>
+            <td><asp:TextBox ID="txtName" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td>Opis:</td>
+            <td><asp:TextBox ID="txtDescription" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <asp:Button ID="btnSave" runat="server" Text="Spremi" OnClick="btnSave_Click" />
+            </td>
+        </tr>
+    </table>
+
+    <asp:GridView ID="GridViewProducts" runat="server" AutoGenerateColumns="True"></asp:GridView>
+</asp:Content>
---
+++ WebForms/Shop.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+namespace YourNamespace
+{
+    public partial class Shop : System.Web.UI.Page
+    {
+        private string connectionString = WebConfigurationManager.ConnectionStrings["ShopDBConnectionString"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGrid();
+            }
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text.Trim();
+            string description = txtDescription.Text.Trim();
+
+            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(description))
+            {
+                Response.Write("<script>alert('Sva polja su obavezna!');</script>");
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Products (Name, Description) VALUES (@Name, @Description)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Name", name);
+                cmd.Parameters.AddWithValue("@Description", description);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+
+            // Osvježavanje grida nakon unosa
+            BindGrid();
+
+            // Brisanje unesenih podataka iz textboxova
+            txtName.Text = string.Empty;
+            txtDescription.Text = string.Empty;
+        }
+
+        private void BindGrid()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Products";
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                GridViewProducts.DataSource = dt;
+                GridViewProducts.DataBind();
+            }
+        }
+    }
+}
