0bdada43b4b4983ad1fff065d8b5900e10aaa1b0
Tomislav Prstec <toprstec@vsite.hr>
2025-01-24 18:43:53 +0100

    lab06 solved tPrstec

---
+++ WebForms/Login.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebForms
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        private readonly SqlConnection _connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ShopDBConnectionString"].ToString());
+        private SqlCommand _command;
+        private SqlDataReader _dr;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Prijava_Click(object sender, EventArgs e)
+        {
+            _connection.Open();
+            _command = new SqlCommand("SELECT * FROM Users Where UserName=@UserName AND Password=@Password", _connection);
+            _command.Parameters.AddWithValue("@UserName", KorisnickoIme.Text);
+            _command.Parameters.AddWithValue("@Password", Lozinka.Text);
+            _dr = _command.ExecuteReader();
+            if (_dr.HasRows)
+            {
+                Response.Redirect("Shop.aspx");
+            }
+            _connection.Close();
+        }
+    }
+}
---
+++ WebForms/Shop.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Shop.aspx.cs" Inherits="WebForms.Shop" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Shop</h1>
+    <br />
+    <br />
+    <br />
+    Naziv: <asp:TextBox ID="Naziv" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    Opis: <asp:TextBox ID="Opis" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    <asp:Button ID="Spremi" runat="server" Text="Spremi" OnClick="Spremi_Click" />
+    <br />
+    <br />
+    <br />
+    <asp:GridView ID="Elementi" runat="server"></asp:GridView>
+</asp:Content>
