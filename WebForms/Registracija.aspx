33bba99d3493c52b6ddab0f2f6de1c527312c02a
Franko Junakovic <franko.junakovic@vsite.hr>
2025-01-25 17:33:54 +0100

    Završen LAB6

---
+++ WebForms/Registracija.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Registracija.aspx.cs" Inherits="WebForms.Registracija" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Registriraj Se</h1>
+    <br />
+    <br />
+    <br />
+    Korisnicko ime: <asp:TextBox ID="KorisnickoIme" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    Puno ime: <asp:TextBox ID="PunoIme" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    Lozinka: <asp:TextBox ID="Lozinka" runat="server" TextMode="Password"></asp:TextBox>
+    <br />
+    <br />
+    Ponovljena Lozinka: <asp:TextBox ID="PonovljenaLozinka" runat="server" TextMode="Password"></asp:TextBox>
+    <br />
+    <br />
+    <asp:Button ID="Registracija_" runat="server" Text="Registracija" OnClick="Registracija__Click" />
+</asp:Content>
---
+++ WebForms/Registracija.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebForms
+{
+    public partial class Registracija : System.Web.UI.Page
+    {
+        private readonly SqlConnection _connection = new SqlConnection(ConfigurationManager.ConnectionStrings["AdoNetKontroleConnectionString"].ToString());
+        private SqlCommand _command;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Registracija__Click(object sender, EventArgs e)
+        {
+            _connection.Open();
+            _command = new SqlCommand("INSERT INTO Users(UserName, Password, FullName) VALUES(@UserName, @Password, @FullName)", _connection);
+            _command.Parameters.AddWithValue("@UserName", KorisnickoIme.Text);
+            _command.Parameters.AddWithValue("@FullName", PunoIme.Text);
+            _command.Parameters.AddWithValue("@Password", Lozinka.Text);
+            _command.ExecuteNonQuery();
+            _connection.Close();
+            Response.Redirect("Login.aspx");
+        }
+    }
+}
