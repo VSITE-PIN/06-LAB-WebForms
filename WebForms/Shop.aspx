854ba84a94d50a23163e08cd9020438326fe56a8
Ivan Ceranac <ivcerana@vsite.hr>
2025-01-28 21:09:42 +0100

    Nadoknada lab. 6. Ivan Ceranac

---
+++ WebForms/Shop.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Shop.aspx.cs" Inherits="WebForms.Shop" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Shop</h1>
+    <br />
+    <br />
+    <br />
+    Naziv: <asp:TextBox ID="Naziv" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    Opis: <asp:TextBox ID="Opis" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    <asp:Button ID="Spremi" runat="server" Text="Spremi" OnClick="Spremi_Click" />
+    <br />
+    <br />
+    <br />
+    <asp:GridView ID="Elementi" runat="server" DataSourceID="SqlDataSource1"></asp:GridView>
+    <asp:SqlDataSource runat="server" ID="SqlDataSource1"></asp:SqlDataSource>
+
+</asp:Content>
---
+++ WebForms/Shop.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebForms
+{
+    public partial class Shop : System.Web.UI.Page
+    {
+        private readonly SqlConnection _connection = new SqlConnection(ConfigurationManager.ConnectionStrings["AdoNetKontroleConnectionString"].ToString());
+        private SqlCommand _command;
+        private SqlDataReader _dr;
+
+        private void Display()
+        {
+            _connection.Open();
+
+            _command = new SqlCommand("SELECT * FROM Products", _connection);
+            _dr = _command.ExecuteReader();
+
+            Elementi.DataSource = _dr;
+            Elementi.DataBind();
+
+            _dr.Close();
+            _connection.Close();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Display();
+        }
+
+        protected void Spremi_Click(object sender, EventArgs e)
+        {
+            _connection.Open();
+            _command = new SqlCommand("INSERT INTO Products(Name, Description) VALUES(@name,@description)", _connection);
+            _command.Parameters.AddWithValue("@name", Naziv.Text);
+            _command.Parameters.AddWithValue("@description", Opis.Text);
+            _command.ExecuteNonQuery();
+            _connection.Close();
+            Display();
+        }
+
+    }
+}
